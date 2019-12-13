db199495703e45bc878c027a56ae8949e85355b2
KarpovM <kmv1712@gmail.com>
2019-12-13 16:24:06 +0500

    Добавил контролер для работы Измененить личные данные

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -5,9 +5,11 @@ from .views import other_page
 from .views import BBLoginView
 from .views import BBLogoutView
 from .views import profile
+from .views import ChangeUserInfoView
 
 app_name = 'main'
 urlpatterns = [
+        path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
         path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
         path('accounts/profile/', profile, name='profile'),
         path('accouns/login/', BBLoginView.as_view(), name='login'),
