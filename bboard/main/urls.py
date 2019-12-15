375e47b8044f685f4fcd4ed325a8baef5000b236
Mihail <kmv1712@gmail.com>
2019-12-15 21:58:52 +0500

    Добавил шаблон для смены пароля, и ссылку на путь к шаблону стр.569

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -6,9 +6,11 @@ from .views import BBLoginView
 from .views import BBLogoutView
 from .views import profile
 from .views import ChangeUserInfoView
+from .views import BBPasswordChangeView
 
 app_name = 'main'
 urlpatterns = [
+        path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
         path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
         path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
         path('accounts/profile/', profile, name='profile'),
