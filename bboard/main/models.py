472a0e8e02f8c3e053e992ee8478552f47e0860e
KarpovM <kmv1712@gmail.com>
2019-12-20 12:25:01 +0500

    Добавил явное удаление объявлений, оставленных пользователем, при удалении этого пользователя стр.595

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -15,6 +15,11 @@ user_registrated.connect(user_registrated_dispatcher)
 class AdvUser(AbstractUser):
     is_activated = models.BooleanField(default=True, db_index=True, verbose_name='Прошел активацию?')
     send_messages = models.BooleanField(default=True, verbose_name='Слать оповещения о новых комментариях?')
+    
+    def delete(self, *args, **kwargs):
+        for bb in self.bb_set.all():
+            bb.delete()
+        super().delete(*args, **kwargs)
 
     class Meta(AbstractUser.Meta):
         pass
