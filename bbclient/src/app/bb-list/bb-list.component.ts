f40fc20558f22b97ffceae96c83087021ef4add3
KarpovM <kmv1712@gmail.com>
2019-12-26 16:18:07 +0500

    Подготовка к разработке тестового веб-сайта часть.4 BbListComponent стр.640

--- bbclient/src/app/bb-list/bb-list.component.ts
+++ bbclient/src/app/bb-list/bb-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BbService } from '../bb.service';
+
+//Компонент выводящий списокобъявлений
+
+
 @Component({
   selector: 'app-bb-list',
   templateUrl: './bb-list.component.html',
   styleUrls: ['./bb-list.component.css']
 })
 export class BbListComponent implements OnInit {
+  private bbs: Object[];
 
-  constructor() { }
+  constructor(private bbservice: BBService) { }
 
   ngOnInit() {
+  	this.bbservice.getBbs().subscribe(
+		(bbs: Object[]) => {this.bbs = bbs;}
+	);
   }
 
 }
