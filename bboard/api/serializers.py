47b1639466b8738772b7f17d569231fd70835f40
KarpovM <kmv1712@gmail.com>
2019-12-26 09:49:02 +0500

    Реализовал вывод и добавление комментариев веб-службу стр.624

--- bboard/api/serializers.py
+++ bboard/api/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 
-from main.models import Bb
+from main.models import Bb, Comment
 
 class BbSerializer(serializers.ModelSerializer):
     """Сериализатор, который будет формировать список рубрик"""
     class Meta:
         model = Bb
         fields = ('id', 'title', 'content', 'price', 'created_at')
-        
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+        Сериализатор, который будет отправлять список комментариев и добавлять
+        новые комментарии.
+    """
+    class Meta:
+        model = Comment
+        fields = ('bb', 'author', 'content', 'created_at')
+
--- bboard/api/urls.py
+++ bboard/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import bbs
+from .views import bbs, comments
 
 urlpatterns = [
+        path('bbs/<int:pk>/comments/', comments),
         path('bbs/', bbs),
 ]
