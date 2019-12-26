d2b7de360d4214b2d93e89ab724f71ef1ce2ad8a
KarpovM <kmv1712@gmail.com>
2019-12-26 14:58:20 +0500

    Подготовка к разработке тестового веб-сайта часть.2 AppComponent стр.632

--- bbclient/src/app/app.module.ts
+++ bbclient/src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BbListComponent } from './bb-list/bb-list.component';
 import { BbDetailComponent } from './bb-detail/bb-detail.component';
+import { BbService } from './bb.service';
+
+registerLocaleData(localeRu, 'ru');
+
+const appRoutes: Routes = [
+	{path: ':pk', component: BdDetailComponent},
+	{path: '', component: BbListComponent}
+];
 
 @NgModule({
   declarations: [
@@ -13,10 +27,16 @@ import { BbDetailComponent } from './bb-detail/bb-detail.component';
     BbDetailComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
+    HttpClienModule,
+    FormsModule
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+  	{provide: LOCALE_ID, useValue: 'ru'},
+	BbService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
