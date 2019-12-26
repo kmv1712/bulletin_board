e263348a698c286fac6674872163cde7678f0cd2
KarpovM <kmv1712@gmail.com>
2019-12-26 15:53:04 +0500

    Подготовка к разработке тестового веб-сайта часть.3 BBService стр.637

--- bbclient/src/app/bb.service.ts
+++ bbclient/src/app/bb.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BbService {
+  private urlPrefix: String = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  getBbs(): Observable<Object[]> {
+	  return this.http.get<Object[]>(`${this.urlPrefix}/api/bbs`);
+  }
+
+  getBb(pk: Number): Observable<Object> {
+	  return this.http.get<Object>(`${this.urlPrefix}/api/bbs/${pk}`);
+	}
+
+  getComments(pk: Number): Observable<Object[]>{
+	  return this.http.get<Object[]>(
+		  `${this.urlPrefix}/api/bbs/${pk}/comments/`);
+		  }
+
+  handleError() {
+  	return (error: any): Observable<Object> => {
+		window.alert(error.message);
+		return of(null);
+		}
+		}
+
+  addComment(bb, author, password, content): Observable<Object> {
+	  const comment = {'bb': bb, 'author': author, 'content': content};
+	  const httpOptions = {
+	  	{ 'Content-Type': 'application/json',
+		'Authorization': 'Basic ' + window.btoa(author + ':' + password) }
+		)
+		};
+		return this.http.post<Object>(
+			`${this.urlPrefix}/api/bbs/${bb}/comments/`,
+			comment, httpOptions).pipe(catchError(this.handleError()));
+			}
+			}
+
+
+
 
   constructor() { }
 }
